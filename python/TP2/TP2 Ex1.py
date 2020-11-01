43d453cd6b6a8d78e00a2eaf55eca02eec8bfa14
Brandon Champenois <72888157+BrandidiCH4O4@users.noreply.github.com>
2020-11-01 17:08:32 +0100

    Delete TP2 Ex1.py

--- python/TP2/TP2 Ex1.py
+++
@@ -1,9 +0,0 @@
-"EX1"
-a=float(input('Quel valeur de AB voulez-vous ? '))
-b=float(input('Quel valeur de AC voulez-vous ? '))
-c=float(input('Quel valeur de BC voulez-vous ? '))
-
-if a<b+c and b<a+c and c<a+b:
-    print("Ceci est un triangle.")
-else:
-    print("Ceci n'est pas un triangle")
