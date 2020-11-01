79d4bb008335b03477f0cdf6fff5806fef0742ad
Brandon Champenois <72888157+BrandidiCH4O4@users.noreply.github.com>
2020-11-01 17:08:40 +0100

    Delete TP2 Ex2.py

--- python/TP2/TP2 Ex2.py
+++
@@ -1,7 +0,0 @@
-"Ex 2"
-x=float(input('Quelle est la valeur de x ? '))
-y=float(input('Quelle est la valeur de y ? '))
-z=float(input('Quelle est la valeur de z ? '))
-
-a=int(max(x, y, z))
-print('La valeur maximum est:',a)
